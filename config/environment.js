5c256ebbf537507633e9f6482298fc1742d8092e
patsul12 <patsul12@gmail.com>
2016-03-03 19:45:00 -0800

    install gmaps, emberfire, etc.. set up basic file structure and transition to pod layout

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'meteorite-landings',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://meteorite-landings.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
