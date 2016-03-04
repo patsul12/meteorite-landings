aa951e082817d2d7635773c46ce54d127dc5bc67
patsul12 <patsul12@gmail.com>
2016-03-03 20:21:39 -0800

    Set up google maps api

--- app/components/main-map/component.js
+++ app/components/main-map/component.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+import GMaps from 'npm:gmaps';
 
 export default Ember.Component.extend({
+  map: null,
+
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      var newMap = new GMaps({
+        div: "#main-map",
+        lat: 0.00,
+        lng: 0.00, 
+        zoom: 3
+      });
+      this.set('map', newMap);
+    });
+  }
 });
